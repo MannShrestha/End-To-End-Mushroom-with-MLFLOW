f9e065e3feb13c7e52076c5e973a0c3d44c242e1
MannShrestha <mannshrestha01@gmail.com>
2025-08-08 00:50:34 +0545

    Data Ingestion phase.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from Mushroom_classifier import logger
 
-logger.info(f"Create your first logging.")
+from Mushroom_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
