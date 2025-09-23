82be394cea63784d43396e43cec4f115e7cbf8c3
MannShrestha <mannshrestha01@gmail.com>
2025-09-24 01:18:07 +0545

    Updata main.py for data transformation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Mushroom_classifier import logger
 
 from Mushroom_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Mushroom_classifier.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from Mushroom_classifier.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -23,6 +24,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
     obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
