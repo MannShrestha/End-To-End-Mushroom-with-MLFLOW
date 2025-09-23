db17411c1423744e1136263077afe11952037976
MannShrestha <mannshrestha01@gmail.com>
2025-09-24 01:08:10 +0545

    Update data transformation pipeline.

---
+++ src/Mushroom_classifier/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,31 @@
+from src.Mushroom_classifier.components.data_transformation import DataTransformation
+from src.Mushroom_classifier import logger
+from src.Mushroom_classifier.config.configuration import ConfigurationManager
+from pathlib import Path
+import os
+
+
+STAGE_NAME = "Data Transformation stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                status = f.read().split(" ")[-1]
+            
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config = data_transformation_config)
+                data_transformation.label_encoding_to_all_feature()
+                data_transformation.train_test_spliting()
+            
+            else:
+                raise Exception("You data schema is not valid")
+            
+        except Exception as e:
+            print(e)
