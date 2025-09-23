3e85ae7576ac5ccb2271208f2b9cdb572eaef821
MannShrestha <mannshrestha01@gmail.com>
2025-09-24 00:53:54 +0545

    Update src configuration managet

--- src/Mushroom_classifier/config/configuration.py
+++ src/Mushroom_classifier/config/configuration.py
@@ -3,7 +3,8 @@ from src.Mushroom_classifier.utils.common import read_yaml, create_directories
 
 
 from Mushroom_classifier.entity.config_entity import (DataIngestionConfig,
-                                                        DataValidationConfig,)
+                                                        DataValidationConfig,
+                                                        DataTransformationConfig)
 
 
 class ConfigurationManager:
@@ -51,5 +52,20 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir = config.root_dir,
+            data_path = config.data_path,
+        )
+
+        return data_transformation_config
+    
 
 
